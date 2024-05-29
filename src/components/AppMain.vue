56dc7b9b80c6d23d7de2562164c244b617a9ce1e
titoni-andrea <andreatitoni@gmail.com>
2024-05-29 10:05:42 +0200

    fumetti inseriti

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,25 +1,143 @@
 <script>
 
+export default {
+    data() {
+        return {
+            fumetti: [
+                {
+                    thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    price: "$19.99",
+                    series: "Action Comics",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    price: "$3.99",
+                    series: "American Vampire 1976",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    price: "$16.99",
+                    series: "Aquaman",
+                    type: "graphic novel",
+                },
+                {
+                    thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    price: "$2.99",
+                    series: "Batgirl",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    price: "$3.99",
+                    series: "Batman",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    price: "$2.99",
+                    series: "Batman Beyond",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    price: "$3.99",
+                    series: "Batman/Superman",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    price: "$4.99",
+                    series: "Batman/Superman Annual",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    price: "$5.99",
+                    series: "Batman: The Joker War Zone",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    price: "$6.99",
+                    series: "Batman: Three Jokers",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    price: "$4.99",
+                    series: "Batman: White Knight Presents: Harley Quinn",
+                    type: "comic book",
+                },
+                {
+                    thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    price: "$16.99",
+                    series: "Catwoman",
+                    type: "graphic novel",
+                },
+            ],
+            cardSelezionata: 0,
+        }
+    },
+
+    methods: {
+
+        scegliChat(indice) {
+            this.chatSelezionata = indice
+        },
+    },
+}
+
 </script>
 
 <template>
     <main>
+        <div class="jumbo">
+        </div>
         <div class="cont-flex">
-            <h2>Il contenuto va qui</h2>
+            <div v-for="elemento, i in fumetti" class="card">
+                <p class="tit-fumetto">{{ fumetti[i].series }}</p>
+                <img :src="fumetti[i].thumb" alt="">
+            </div>
+
         </div>
     </main>
 </template>
 
 <style scoped>
 main {
-
     background-color: black;
     color: white;
-    padding: 2rem 0px;
 }
+
+.jumbo {
+    width: 100%;
+    height: 400px;
+    background-image: url("/public/jumbotron.jpg");
+
+}
+
+.card {
+    width: 14.9%;
+    object-fit: contain;
+    overflow: hidden;
+    aspect-ratio: 1;
+    color: white;
+    margin-bottom: 30px;
+}
+
+.card img {
+    width: 100%;
+}
+
 
 .cont-flex {
     width: 1200px;
     margin: auto;
+    padding: 1.5rem 0px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 25px;
 }
 </style>
